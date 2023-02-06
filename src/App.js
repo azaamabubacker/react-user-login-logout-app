44cfcdc11927e7e6f16a0597511534d6a5817e02
azaamabubacker <cleancodelk@gmail.com>
2023-02-06 21:38:59 +0530

    LocalStorage to save user credentials

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
-import Navigation from "./components/MainHeader/Navigation";
+import React, { useEffect, useState } from "react";
+
+import MainHeader from "./components/MainHeader/MainHeader";
 import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const locStorageInfo = localStorage.getItem("isLoggedIn");
+
+    if (locStorageInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div>
-      <Navigation />
-      <Login />
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
